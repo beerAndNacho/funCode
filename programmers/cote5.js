1c911aa724d2b89d011e3f70fd674f229e19af62
joshK <luckykkh7@gmail.com>
2022-06-09 15:48:50 +0900

    문자열 압축 문제풀이중

--- programmers/cote5.js
+++ programmers/cote5.js
@@ -3,29 +3,49 @@ function solution(s) {
 
     var answer = 0;
     
-    let str ='';
-    let cnt ='';
+    // let str ='';
+    // let cnt ='';
 
-    for(let i =0; i<s.length; i++){
+    // for(let i =0; i<s.length; i++){
 
-        const strSlice = s.slice(0,(i+1));
-        let ii = i+i;
-        if(i===0) {
-            ii = 1;
-        }
-        // console.log(s.slice(i,ii));
+    //     if(i===0) {
+    //         i = 1;
+    //         ii = i*2;
+    //     }else {
+    //         ii = i*2
+    //     }
+    //     const strSlice = s.slice(0,(i));
 
-        if(strSlice ==='abc') {
-            console.log(s.slice(i,ii));
-            console.log(strSlice);
-        }
-        // if(strSlice === s.slice(i,ii)){
-        //     console.log(strSlice);
-        // }
-       
+    //     // console.log('i는->',i, strSlice);
+    //     // console.log('i는',i,'ii는',ii,'->  ',s.slice(i,ii));
+
+    //     // if(strSlice ==='abc') {
+    //     // }
+
+    //     if(strSlice === s.slice(i,ii)){
+    //         // console.log('i는',i, 'ii는',ii);
+
+    //         let diff = ii - i;
+
+    //         for(let z=0; z<s.length; z+=diff) {
+
+    //             let zz = z+diff;
+
+    //             if(strSlice === s.slice(z, zz)) {
+
+    //                 console.log(s.slice(z, zz));
+
+    //             } else {
+    //               break;
+    //             }
+    //         }
+
+    //     }
+
+
     }
     return answer;
 }
 
-const s = "abcabcabcabcdededededede";
+const s = "abcabcabcabcdededededede"
 solution(s);
